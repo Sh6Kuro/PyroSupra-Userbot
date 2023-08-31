e66c03d7ac290db6c6cd6202da6f6f3457776163
Sh6Kuro <135512876+Sh6Kuro@users.noreply.github.com>
2023-09-01 02:35:01 +0700

    Add files via upload

---
+++ Sh6Project/utils/sections.py
@@ -0,0 +1,33 @@
+# Credits: @cmrkuro
+# Copyright (C) 2023 PyroSupra-Userbot
+#
+# This file is a part of < https://github.com/Sh6Kuro/PyroSupra-Userbot/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/Sh6Kuro/PyroSupra-Userbot/blob/main/LICENSE/>.
+#
+# t.me/cemarasupport & t.me/Kat4logXcode
+
+n = "\n"
+w = " "
+
+bold = lambda x: f"**{x}:** "
+bold_ul = lambda x: f"**--{x}:**-- "
+
+mono = lambda x: f"`{x}`{n}"
+
+
+def section(
+    title: str,
+    body: dict,
+    indent: int = 2,
+    underline: bool = False,
+) -> str:
+    text = (bold_ul(title) + n) if underline else bold(title) + n
+
+    for key, value in body.items():
+        text += (
+            indent * w
+            + bold(key)
+            + ((value[0] + n) if isinstance(value, list) else mono(value))
+        )
+    return text
