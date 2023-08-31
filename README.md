dae601ffe0398d14b39dca14bcdcdcd12e2660dc
Sh6Kuro <135512876+Sh6Kuro@users.noreply.github.com>
2023-09-01 02:30:33 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,34 @@
+<p align="center">
+  <img src="https://telegra.ph//file/a995a2df70cfdb09a6dc3.jpg">
+</p>
+
+<h4><p align="center"> ᴘʏʀᴏꜱᴜᴘʀᴀ-ᴜꜱᴇʀʙᴏᴛ </p></h4>
+
+<p align="center"> DEAK TANGGUNG SENDIRI YA KNTL.</p>
+
+<p align="center">
+<a href="https://t.me/cmrkuro"> <img src="https://img.shields.io/badge/Cmr-Kuro-blue?&logo=telegram" alt="KURO on Telegram" /> </a><br>
+<a href="https://python-telegram-bot.org"> <img src="https://img.shields.io/badge/PTB-13.13-white?&style=flat-round&logo=github" alt="Python Telegram Bot" /> </a>
+<a href="https://github.com/psf/black"><img alt="Code style: black" src="https://img.shields.io/badge/code%20style-black-000000.svg"></a><br>
+<a href="https://docs.telethon.dev"> <img src="https://img.shields.io/badge/Telethon-1.24.0-red?&style=flat-round&logo=github" alt="Telethon" /> </a>
+<a href="[https://docs.python.org"> <img src="https://img.shields.io/badge/Join-donasi%20dana-blue?style=for-the-badge&logo=DANA](https://img.shields.io/badge/Join-donasi%20dana-blue?style=for-the-badge&logo=DANA" alt="Dana" /> </a><br>
+<a href="https://makeapullrequest.com"> <img src="https://img.shields.io/badge/PRs-Welcome-blue.svg?style=flat-round" alt="PRs" /> </a>
+</p>
+
+### TUTORIAL
+
+- First fork this repository.
+- Then change the deploy link bellow.
+- This step must be doing because mine link is violates the salesforce acceptable use.
+
+<p align="center"><a href="https://dashboard.heroku.com/new?template=https://github.com/Sh6Kuro/PyroSupra-Userbot"> <img 
+src="https://img.shields.io/badge/Deploy%20To%20Heroku-red?style=flat&logo=heroku" width="210" height="34.45" /></a></p>
+
+
+```
+
+
+## Credit 
+
+```
+```
---
+++ config.py
@@ -0,0 +1,49 @@
+# Credits: @mrismanaziz
+# Copyright (C) 2022 Pyro-ManUserbot
+#
+# This file is a part of < https://github.com/mrismanaziz/PyroMan-Userbot/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/mrismanaziz/PyroMan-Userbot/blob/main/LICENSE/>.
+#
+# t.me/SharingUserbot & t.me/Lunatic0de
+
+from base64 import b64decode
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID", ""))
+BLACKLIST_CHAT = getenv("BLACKLIST_CHAT", None)
+if not BLACKLIST_CHAT:
+    BLACKLIST_CHAT = [-1001473548283, -1001810928340 , -1001821201567, -1001812143750 , -1001982790377]
+BLACKLIST_GCAST = {int(x) for x in getenv("BLACKLIST_GCAST", "").split()}
+BOTLOG_CHATID = int(getenv("BOTLOG_CHATID") or 0)
+BOT_VER = "0.2.0@main"
+BRANCH = "main"
+CHANNEL = getenv("CHANNEL", "Kat4logXcode")
+CMD_HANDLER = getenv("CMD_HANDLER", ".")
+DB_URL = getenv("DATABASE_URL", "")
+GIT_TOKEN = getenv(
+    "GIT_TOKEN",
+    b64decode("Z2hwX2xYRzdPQUlnenpWTXYxWEt2VzdLa0ZGa0xmYVBRYzRKcWFPbw==").decode(
+        "utf-8"
+    ),
+)
+GROUP = getenv("GROUP", "cemarasupport")
+HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
+HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
+PMPERMIT_PIC = getenv("PMPERMIT_PIC", None)
+REPO_URL = getenv(
+    "REPO_URL",
+    b64decode("aHR0cHM6Ly9naXRodWIuY29tL2tlbnNrdXkvUFlST1RBTg==").decode("utf-8"),
+)
+STRING_SESSION1 = getenv("STRING_SESSION1", "")
+STRING_SESSION2 = getenv("STRING_SESSION2", "")
+STRING_SESSION3 = getenv("STRING_SESSION3", "")
+STRING_SESSION4 = getenv("STRING_SESSION4", "")
+STRING_SESSION5 = getenv("STRING_SESSION5", "")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
