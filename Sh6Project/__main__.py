963ed4d3b0404b6c76d370cecc025168e6f6640c
Sh6Kuro <135512876+Sh6Kuro@users.noreply.github.com>
2023-09-01 02:33:20 +0700

    Add files via upload

--- Sh6Project/__main__.py
+++ Sh6Project/__main__.py
@@ -1,1 +1,60 @@
+# Credits: @cmrkuro
+# Copyright (C) 2023 PyroSupra-Userbot
+#
+# This file is a part of < https://github.com/Sh6Kuro/PyroSupra-Userbot/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/Sh6Kuro/PyroSupra-Userbot/blob/main/LICENSE/>.
+#
+# t.me/cemarasupport & t.me/Kat4logXcode
 
+import importlib
+
+from pyrogram import idle
+from uvloop import install
+
+from config import BOT_VER, CMD_HANDLER
+from Sh6Project import BOTLOG_CHATID, LOGGER, LOOP, aiosession, bot1, bots
+from Sh6Project.helpers.misc import create_botlog, heroku
+from Sh6Project.modules import ALL_MODULES
+
+MSG_ON = """
+🔥 **PyroSupra-Userbot Berhasil Di Aktifkan**
+━━
+➠ **Userbot Version -** `{}`
+➠ **Ketik** `{}alive` **untuk Mengecheck Bot**
+━━
+"""
+
+
+async def main():
+    for all_module in ALL_MODULES:
+        importlib.import_module(f"Sh6Project.modules.{all_module}")
+    for bot in bots:
+        try:
+            await bot.start()
+            bot.me = await bot.get_me()
+            await bot.join_chat("Kat4logXcode")
+            await bot.join_chat("cemarasupport")
+            try:
+                await bot.send_message(
+                    BOTLOG_CHATID, MSG_ON.format(BOT_VER, CMD_HANDLER)
+                )
+            except BaseException:
+                pass
+            LOGGER("Sh6Project").info(
+                f"Logged in as {bot.me.first_name} | [ {bot.me.id} ]"
+            )
+        except Exception as a:
+            LOGGER("main").warning(a)
+    LOGGER("Sh6Project").info(f"PyroSupra-UserBot v{BOT_VER} [🔥 BERHASIL DIAKTIFKAN! 🔥]")
+    if bot1 and not str(BOTLOG_CHATID).startswith("-100"):
+        await create_botlog(bot1)
+    await idle()
+    await aiosession.close()
+
+
+if __name__ == "__main__":
+    LOGGER("Sh6Project").info("Starting PyroSupra-UserBot")
+    install()
+    heroku()
+    LOOP.run_until_complete(main())
