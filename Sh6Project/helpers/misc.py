e8f42be14d90611ee16eec2dd38a78dd7d211de4
Sh6Kuro <135512876+Sh6Kuro@users.noreply.github.com>
2023-10-26 23:48:21 +0700

    Update misc.py

--- Sh6Project/helpers/misc.py
+++ Sh6Project/helpers/misc.py
@@ -128,7 +128,7 @@ async def create_botlog(client):
     LOGGER("ProjectMan").info(
         "TUNGGU SEBENTAR. SEDANG MEMBUAT GROUP LOG USERBOT UNTUK ANDA"
     )
-    desc = "Group Log untuk PyroMan-UserBot.\n\nHARAP JANGAN KELUAR DARI GROUP INI.\n\n✨ Powered By ~ @Lunatic0de ✨"
+    desc = "Group Log untuk PyroSupra-UserBot.\n\nHARAP JANGAN KELUAR DARI GROUP INI.\n\n✨ Powered By ~ @Kat4logXcode ✨"
     try:
         gruplog = await client.create_supergroup("Log UserBot", desc)
         if await in_heroku():
